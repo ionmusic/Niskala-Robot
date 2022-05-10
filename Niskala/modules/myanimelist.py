f69e2c60d7b739986b71bd6de00edb3862d53a86
Rizky_Yudha <102364151+Rzydx@users.noreply.github.com>
2022-05-10 13:54:08 +0700

    Update myanimelist.py

--- Niskala/modules/myanimelist.py
+++ Niskala/modules/myanimelist.py
@@ -14,7 +14,7 @@ from telegram import (
 )
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 
-from Yuriko import dispatcher
+from Niskala import dispatcher
 
 jikan = Jikan()
 
