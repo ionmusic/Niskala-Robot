b393f63c9718eaffcb2a7817fee8c4123362cec5
Rizky_Yudha <102364151+Rzydx@users.noreply.github.com>
2022-09-10 11:13:29 +0700

    Update tools.py

--- Niskala/modules/tools.py
+++ Niskala/modules/tools.py
@@ -23,18 +23,7 @@ __help__ = """
 ✗  /truth - `challenge`
 ✗  /dare - `challenge`
 
-*Link To File:*
-✗  /up - `reply to a direct download link to upload it to telegram as files`
- 
- *File To Link:*
-✗  /transfersh - `reply to a telegram file to upload it on transfersh and get direct download link`
-✗  /tmpninja - `reply to a telegram file to upload it on tmpninja and get direct download link`
-
- *File Or Text To Telegraph:*
-✗  /tgm - `Get Telegraph Link Of Replied Media`
-✗  /tgt - `Get Telegraph Link of Replied Text`
-
- *Tagger*
+*Tagger*
 ✗  /tagall - `for tag everyone at least 100 member first.`
 
 *Maintained By : @IDnyaAL!*
