738df35e16a0962d8c9368f7a059bfa74973327b
Rizky_Yudha <102364151+Rzydx@users.noreply.github.com>
2022-09-10 11:03:13 +0700

    Create truthordare_help.py

---
+++ Niskala/modules/truthordare_help.py
@@ -0,0 +1,12 @@
+__mod_name__ = "Tʀᴜᴛʜ/Dᴀʀᴇ"
+
+__help__ = """
+
+Truth Or Dare is a Word Game Module in the form of Questions and Challenges
+This Module is like Truth Or Dare Bot
+
+Command :
+❂ /truth - Question
+❂ /dare - Challenges
+
+Notes : If you have suggestions for words for truth or dare, you can Pc @IDnyaAL so that they can be added.
