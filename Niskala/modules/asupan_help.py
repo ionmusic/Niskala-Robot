59db31af7abbca3af5fad1cbd09ba98d1f6951af
Rizky_Yudha <102364151+Rzydx@users.noreply.github.com>
2022-09-10 11:21:27 +0700

    Create asupan_help.py

---
+++ Niskala/modules/asupan_help.py
@@ -0,0 +1,10 @@
+__mod_name__ = "Asᴜᴘᴀɴ"
+
+__help__ = """
+Command for Asupan Module:
+❂ /ngaji - Looking for the qur'an to listen
+❂ /asupan - Searching Intake Tiktok Videos
+❂ /ayang - Looking for Ayang's Photos
+❂ /ppcp - Looking for PP Couple For Your Bhestie
+
+Warning ️: This Module is For Entertainment Only !!!"""
